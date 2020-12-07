0e85e398b0eec05b414a6765cf04571e510a1fa2
Pham, Son Tung <pham.sontung@hotmail.de>
2020-12-07 02:29:18 +0100

    tvc

---
+++ build/CMakeFiles/thuvienchia.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thuvienchia.dir/phepnhan.cpp.o"
+  "libthuvienchia.pdb"
+  "libthuvienchia.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thuvienchia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ phepnhan.h
@@ -0,0 +1,13 @@
+#ifndef PHEPNHAN_H
+#define PHEPNHAN_H
+
+class phepnhan
+{
+public:
+    phepnhan(const int a, const int b);
+    int get();
+private:
+    int m_a, m_b, m_re;
+};
+
+#endif // PHEPNHAN_H
